e71f5fcfac66a868ff3ebcafaff00be9f80bc049
Skyler Nelles <snelles@gmail.com>
2014-02-15 14:27:56 -0500

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brewbrain::Application.config.secret_token = '4d5502a27704fce05f9fe060c9e731cdae491abdef480fc1de74a43494b3b9d0febb54f23b0ac984501a57e3effbfb3faac427b58a5ec4bd79e81b33cede4af9'
